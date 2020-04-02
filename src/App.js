a7341470dce744da16469d2aea8dbda1c5153707
MeghaSharma33 <megha.sharma3333@gmail.com>
2020-04-02 14:43:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Menu from "./components/MenuComponents";
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Menu>
+
+      </Menu>
+     
+    </div>
+  );
+}
+
+export default App;
